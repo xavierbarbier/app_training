56d504a912024d34ee80939552661ea1b93fc64d
Xavier Barbier <contact@xavierbarbier.com>
2021-01-27 17:23:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+server.secret_key = os.environ.get(‘SECRET_KEY’, ‘my-secret-key’)
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
