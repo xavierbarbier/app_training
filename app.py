0511688ebea73ed78aac64221a108ee585ae1cbf
Xavier Barbier <contact@xavierbarbier.com>
2021-01-27 17:35:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -8,21 +10,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-top_markdown_text = '''
-This is my first deployed app
-'''
-
 app.layout = html.Div([
-
-    dcc.Markdown(children=top_markdown_text),
-
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
 ])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-
-
-
