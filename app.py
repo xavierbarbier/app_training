e9c54a34cd9460b5abf85a9aa167bae4a737b0e1
Xavier Barbier <contact@xavierbarbier.com>
2021-01-28 13:59:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,25 +13,33 @@ server = app.server
 app.layout = html.Div([
     html.H2('Hello World'),
     
-    dcc.Textarea(
-    id = 'title'
-    placeholder='Enter a title for your question...',
-    style={'width': '100%'}),
+    html.Div(["Input: ",     
+        dcc.Textarea(id='title',
+        placeholder='Enter a title for your question...',
+        style={'width': '100%'})]),
     
-    dcc.Textarea(
-    id = 'body'
-    placeholder='Enter a body for your question...',
-    style={'width': '100%'}),
+    html.Br(),
+    
+    html.Div(["Input: ",     
+        dcc.Textarea(id='body',
+        placeholder='Enter a body for your question...',
+        style={'width': '100%'})]),
+    
+    html.Br(),
     
     html.Button('Submit', id='button'),
+              
+    html.Div(id='my-output')
     
     
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('title', 'value')])
-def display_value(value):
-    return value
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='title', component_property='value')
+)
+def update_output_div(input_value):
+    return 'Output: {}'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
