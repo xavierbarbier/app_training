43efa7197b7eb921ea7766d903f528787bb152f6
Xavier Barbier <contact@xavierbarbier.com>
2021-01-28 14:32:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
+import os
+
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-from dash.exceptions import PreventUpdate
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div([
-    html.Button('Click here to see the content', id='show-secret'),
-    html.Div(id='body-div')
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
 ])
 
-@app.callback(
-    Output(component_id='body-div', component_property='children'),
-    Input(component_id='show-secret', component_property='n_clicks')
-)
-def update_output(n_clicks):
-    if n_clicks is None:
-        raise PreventUpdate
-    else:
-        return "Elephants are the only animal that can't jump"
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
