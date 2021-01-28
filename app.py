d0888432196455aeb8822e62fb4c226c5361bfc0
Xavier Barbier <contact@xavierbarbier.com>
2021-01-28 16:31:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,27 +31,7 @@ app.layout = html.Div([
     
 ])
 
-@app.callback(
-    dash.dependencies.Output('the-title', 'children'),
-    [dash.dependencies.Input('button', 'n_clicks')],
-    [dash.dependencies.State('my-title', 'value')],
-    
-def update_title(n_clicks,value):
-    if n_clicks is None:
-        raise PreventUpdate
-    else:
-        return 'Output title: {}'.format(value)
-    
-@app.callback(
-    dash.dependencies.Output('the-body', 'children'),
-    [dash.dependencies.Input('button', 'n_clicks')],
-    
-    [dash.dependencies.State('my-body', 'value')])
-def update_body(n_clicks,value):
-    if n_clicks is None:
-        raise PreventUpdate
-    else:
-        return 'Output body: {}'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
