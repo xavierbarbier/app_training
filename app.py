7800c37d7c2058eeca2114e9b0b664bd4375f2a1
Xavier Barbier <contact@xavierbarbier.com>
2021-01-28 16:19:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,27 @@ server = app.server
 app.layout = html.Div([
     html.H2('Hello World'),
         
-    html.Div(["Title: ",
-              dcc.Input(id='my-title', value='initial value', type='text',style={'width': '100%'})]),
+    html.Div([
+        
+    "Title: ",dcc.Input(id='my-title', value='initial value', type='text',style={'width': '100%'}),
+        
     html.Br(),
-    html.Div(["Body: ",
-              dcc.Input(id='my-body', value='initial value', type='text',style={'width': '100%'})]),
+        
+    "Body: ",dcc.Input(id='my-body', value='initial value', type='text',style={'width': '100%'}),
+        
     html.Br(),
     
     html.Button('Submit', id='button',n_clicks = 0),
     
-    html.Div(id='my-output')
+    html.Div(id='my-output')]
     
 ])
 
 @app.callback(
     dash.dependencies.Output('my-output', 'children'),
     [dash.dependencies.Input('button', 'n_clicks')],
-    [dash.dependencies.State('my-title', 'value')])
+    [dash.dependencies.State('my-title', 'value')],
+    [dash.dependencies.State('my-body', 'value')])
 def update_output(n_clicks,input1, input2):
     if n_clicks is None:
         raise PreventUpdate
