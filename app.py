f1570b2830f54e48739ab877f40e0013279ebffa
Xavier Barbier <contact@xavierbarbier.com>
2021-01-28 14:57:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,23 @@ app.layout = html.Div([
     html.Div(["Body: ",
               dcc.Input(id='my-body', value='initial value', type='text',style={'width': '100%'})]),
     html.Br(),
+    
+    html.Button('Submit', id='button'),
+    
     html.Div(id='my-output')
     
 ])
 
 @app.callback(
     dash.dependencies.Output(component_id='my-output', component_property='children'),
+    [dash.dependencies.Input('button', 'n_clicks')]
     [dash.dependencies.Input(component_id='my-title', component_property='value')]
 )
 def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+    if n_clicks is None:
+        raise PreventUpdate
+    else :
+        return 'Output: {}'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
