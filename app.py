a04239b2af5c9831098666dd217e5b90c4041c49
Xavier Barbier <contact@xavierbarbier.com>
2022-04-02 14:16:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,82 +7,27 @@ import base64
 import joblib 
 import os
 import numpy as np
-import emoji
-import nltk
-from nltk.corpus import stopwords
-from nltk.util import ngrams
-from bs4 import BeautifulSoup
 import re
 import pandas as pd
-import requests
-import tweepy
-#import time
-#import tensorflow as tf
-#from tensorflow import keras
-#from transformers import TFAutoModelForSequenceClassification
-#from transformers import AutoTokenizer
-#from transformers import TextClassificationPipeline
+
+
 import plotly.express as px
-from textblob import TextBlob
-from textblob_fr import PatternTagger, PatternAnalyzer
 
 
+data = pd.read_csv("datatweets_sentiments.csv")
 
-twitter_candidats = {               
-                     "Philippe Poutou" : "PhilippePoutou",
-                     "Nathalie Arthaud" : "n_arthaud",
-                     "Jean-Luc Mélenchon" : "JLMelenchon",
-                     "Fabien Roussel" : "Fabien_Roussel",
-                     "Florian Philippot" : "f_philippot",
-                     "Yannick Jadot" :"yjadot" ,
-                     "Anne Hidalgo" : "Anne_Hidalgo",
-                     "Christiane Taubira" : "ChTaubira",
-                     "Emmanuel Macron" : "EmmanuelMacron",
-                     "Valérie Pecresse" : "vpecresse",
-                     "Jean Lassalle":"jeanlassalle",
-                     "Nicolas Dupont-Aignan" : "dupontaignan",
-                     "Marine Le Pen" : "MLP_officiel",
-                     "Eric Zemmour" : "ZemmourEric",
-                      "François Asselineau" : "UPR_Asselineau",
-                      "Hélène Thouy" : "HeleneThouy"  
-                      } 
-
-
-
-# creating function with previously tested steps
-def text_to_words( raw_text ):
-    
-    # Function to convert a raw text to a string of words
-    # The input is a single string (a raw text), and 
-    # the output is a single string (a preprocessed text)
-    text = BeautifulSoup(raw_text).get_text() 
-
-    text = " ".join(filter(lambda x:x[0]!='@', text.split()))
-
-    text = " ".join(filter(lambda x:x[0]!='#', text.split()))
-
-    text = re.sub(r'http\S+', '', text)
-    
-    text = emoji.demojize(text, delimiters=("", ""), language = "fr")
-
-    text = text.replace("_"," ")
-
-    words = text.lower().split()        
-
-    #lemmatized_words = [stemmer.stem(w) for w in words] 
-    
-    stops = set(stopwords.words("french"))                  
-    
-    meaningful_words = [w for w in words if not w in stops]  
-       
-    return( " ".join( meaningful_words))
-
-
-#### API TWITTER
-
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-# Construct the API instance
-api = tweepy.API(auth)
+candidats = ['Philippe Poutou',
+            'Nathalie Arthaud',
+            'Jean-Luc Mélenchon',
+            'Fabien Roussel',
+            'Yannick Jadot',
+            'Anne Hidalgo',
+            'Emmanuel Macron',
+            'Valérie Pecresse',
+            'Jean Lassalle',
+            'Nicolas Dupont-Aignan',
+            'Marine Le Pen',
+            'Eric Zemmour']
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -102,7 +47,7 @@ app.layout = html.Div([html.H1("Exploration Twitter des candidats(es) à l'élec
                         clearable=True,
                         options=[
                             {'label': name, 'value': name}
-                            for name in twitter_candidats.keys()]),
+                            for name in candidats]),
                        html.Button('Submit', id='submit-val', n_clicks=0),
         html.Div(id='container-button-basic'),
                        html.Div(id='container-button-basic2')
@@ -116,92 +61,12 @@ def update_bar_chart(n_clicks , cand):
   changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
   if 'submit-val' in changed_id: 
     
-    tweet = []
-    date = []
+    temp = data[data["candidat"] == cand]
 
-    ID = twitter_candidats[str(cand)]
-    list_tweets = api.user_timeline(ID, count = 100)
-
-    for t in range(len(list_tweets)):
-      
-      id = list_tweets[t].id
-      status = api.get_status(id, tweet_mode="extended")
-      try:
-          tweet.append(status.retweeted_status.full_text)
-          date.append(status.created_at)
-      except AttributeError:  # Not a Retweet
-          tweet.append(status.full_text)
-          date.append(status.created_at)
-
-    text = []
-    for t in tweet:
-      text.append(text_to_words(t))
-
-    corpus = ' '.join(text)
-
-    words = corpus.split()    
-
-    polarity = []
-    for pub in text:
-      
-      polarity.append(TextBlob(pub,pos_tagger=PatternTagger(),analyzer=PatternAnalyzer()).sentiment[0])
-
-    temp = pd.DataFrame({"Date":date,"Sentiment":polarity, "Tweet":tweet})
-    size = 3
-    temp["size"] = size
-
-    fig = px.scatter(temp, x="Date", y="Sentiment",size = "size",
-                 title='Polarité des sentiments des 100 derniers Tweets (+1: positif | -1: négatif)',
-                 hover_data=["Sentiment", "Tweet"],range_color = [-1,1],
+    fig = px.scatter(temp, x="dates", y="Sentiment",size = "size",
+                 title='Polarité des sentiments des Tweets depuis le 1er janvier 2022 (+1: positif | 0: négatif)',
+                 hover_data=["Sentiment", "tweets"],range_color = [0,1],
                  color = "Sentiment")
-    fig.update_layout(yaxis_range=[-1,1])
-
+    fig.update_layout(yaxis_range=[0,1])
     return html.Div([dcc.Graph(figure=fig)
-                     ])
-# update bar chart #1
-@app.callback(
-    dash.dependencies.Output('container-button-basic2', 'children'),
-  [dash.dependencies.Input('submit-val', 'n_clicks')],
-    [dash.dependencies.Input("candidat",'value')])
-def update_bar_chart2(n_clicks , cand):
-  changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-  if 'submit-val' in changed_id: 
-    ID = twitter_candidats[str(cand)]
-    # replies
-    reponses = []
-    replies_dates = []
-    count = 0  
-    replies = tweepy.Cursor(api.search, q='to:{}'.format(ID),
-                                    #since_id=tweet_id,
-                            tweet_mode='extended').items()
-    for r in replies:
-      if count != 50:
-        status = api.get_status(r.id, tweet_mode="extended")
-        reponses.append(status.full_text)
-        replies_dates.append(status.created_at)
-        count += 1
-      else:
-        break
-    
-    rep_text = []
-    for t in reponses:
-      rep_text.append(text_to_words(t))
-
-    rep_polarity = []
-    for pub in rep_text:
-      
-      rep_polarity.append(TextBlob(pub,pos_tagger=PatternTagger(),analyzer=PatternAnalyzer()).sentiment[0])
-
-    rep_temp = pd.DataFrame({"Date":replies_dates,"Sentiment":rep_polarity, "Tweet":reponses})
-    size = 3
-    rep_temp["size"] = size
-    
-    rep_fig = px.scatter(rep_temp, x="Date", y="Sentiment",size = "size",
-                    title='Polarité des sentiments des 50 dernières réponses (+1: positif | -1: négatif)',
-                    hover_data=["Sentiment", "Tweet"],range_color = [-1,1],
-                    color = "Sentiment")
-    rep_fig.update_layout(yaxis_range=[-1,1])
- 
-
-    return html.Div([dcc.Graph(figure=rep_fig)
                      ])
